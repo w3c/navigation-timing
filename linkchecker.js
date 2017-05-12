1dab4d5749445b07ba581843b23e59f1bbd08552
Marcos Caceres <marcos@marcosc.com>
2017-05-12 13:40:44 +1000

    feat(linkchecker): use stdin, not a file

--- linkchecker.js
+++ linkchecker.js
@@ -1,34 +1,46 @@
+#!/usr/bin/env node
 const path = require("path");
 const { JSDOM } = require("jsdom");
-const fsp = require("fs-promise");
 const colors = require("colors");
 colors.setTheme({
   error: "red",
   info: "green",
+  details: "blue",
 });
 
-async function checkForErrors() {
-  const data = await fsp.readFile(path.join(__dirname, "linkreport.html"), {
-    encoding: "UTF-8",
+console.log(colors.info("☕ Link checker can take about 1-2 mins... \n"));
+
+const ready = new Promise(resolve => {
+  const stdin = process.openStdin();
+  let data = "";
+  stdin.on("data", chunk => {
+    data += chunk;
   });
+  stdin.on("end", () => {
+    resolve(data);
+  });
+});
+
+function checkForErrors(data) {
   const dom = new JSDOM(data);
   const invalidLinks = dom.window.document.querySelectorAll(
     ".report dt[id$='404']"
   );
   if (!invalidLinks.length) {
+    console.log(color.info("👍 is all good!"));
+    process.exit(0);
     return; // all is well
   }
   Array.from(invalidLinks).map(elem =>
     console.error(
       colors.error("💀 " + elem.textContent + "\n"),
       colors.info(elem.nextElementSibling.textContent + "\n"),
-      colors.info(
+      colors.details(
         elem.nextElementSibling.nextElementSibling.textContent + "\n"
       ),
-      "======\n"
+      "\n"
     )
   );
   process.exit(1);
 }
-
-checkForErrors();
+ready.then(checkForErrors);
